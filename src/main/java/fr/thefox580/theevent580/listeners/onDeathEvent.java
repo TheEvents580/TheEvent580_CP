30c681858c143c75244cc1e02e13a6d0f3b25261
Fox <zoelliotmitong@gmail.com>
2023-11-29 22:33:05 +0100

    Multiple updates Added death messages Made the function public and made the game choosing a random message with 10 different message Updated Blue color Inverted color tags when a player kills another player

--- src/main/java/fr/thefox580/theevent580/listeners/onDeathEvent.java
+++ src/main/java/fr/thefox580/theevent580/listeners/onDeathEvent.java
@@ -19,13 +19,13 @@ public class onDeathEvent implements Listener {
         this.advMain = advMain;
     }
 
-    static String[] killMessage = {"'s game stopped working...", " should uninstall the game", " didn't know the game",
-            " fell off", "should go back to studying", " would be better at Fortnite", " did not buy enough shares",
-            " ate an apple", "failed the ready check at the start of the event"}; //Set an Array with death messages
+    static String[] killMessage = {"'s game stopped working...", " should uninstall the game.", " didn't know the game.",
+            " fell off.", " should go back to studying.", " would be better at Fortnite.", " did not buy enough shares.",
+            " ate an apple.", " failed the ready check at the start of the event."}; //Set an Array with death messages
 
-    static String killMessageFinal(){ //Function to choose a random message
+    public static String killMessageFinal(){ //Function to choose a random message
         Random random = new Random(); //Set a random number
-        int randomNumberInArray = random.nextInt(7); //Choose a random number between 0-6
+        int randomNumberInArray = random.nextInt(10); //Choose a random number between 0-9
         return killMessage[randomNumberInArray]; //Returns the String for the message
     }
 
@@ -58,7 +58,7 @@ public class onDeathEvent implements Listener {
             teamVictim = '動'; //Set the tag of the player's team
 
         } else if (victim.hasPermission("group.bleu")) { //If the player is in team blue
-            colorVictim = TextColor.color(85, 85, 255); //Set color of text to blue
+            colorVictim = TextColor.color(0, 0, 170); //Set color of text to blue
             teamVictim = '像'; //Set the tag of the player's team
 
         } else if (victim.hasPermission("group.violet")) { //If the player is in team purple
@@ -76,7 +76,7 @@ public class onDeathEvent implements Listener {
                     .append(Component.text("☠",TextColor.color(255, 85, 85))) //Set custom death message
                     .append(Component.text("] " + teamVictim,TextColor.color(255, 255, 255))) //Set custom death message
                     .append(componentVictim)
-                    .append(Component.text(victim.getName(), colorVictim)) //Set custom death message
+                    .append(Component.text(' '+victim.getName(), colorVictim)) //Set custom death message
                     .append(Component.text(killMessageFinal(),TextColor.color(170, 170, 170))); //Set custom death message
             advMain.adventure().players().sendMessage(message);
         }
@@ -107,7 +107,7 @@ public class onDeathEvent implements Listener {
                 teamKiller = '動'; //Set the tag of the player's team
 
             } else if (killer.hasPermission("group.bleu")) { //If the player is in team blue
-                colorKiller = TextColor.color(85, 85, 255); //Set color of text to blue
+                colorKiller = TextColor.color(0, 0, 170); //Set color of text to blue
                 teamKiller = '像'; //Set the tag of the player's team
 
             } else if (killer.hasPermission("group.violet")) { //If the player is in team purple
@@ -122,10 +122,10 @@ public class onDeathEvent implements Listener {
 
             Component message = Component.text('[') //Setup custom death message
                     .append(Component.text('☠', TextColor.color(255, 85, 85))) /* ☠ */
-                    .append(Component.text("] " + teamKiller, TextColor.color(255, 255, 255))) //I mean
+                    .append(Component.text("] " + teamVictim, TextColor.color(255, 255, 255))) //I mean
                     .append(componentVictim) //Add custom victim head to message
                     .append(Component.text(' ' + victim.getName(), colorVictim)) //Add victim's name
-                    .append(Component.text(" was killed by " + teamVictim, TextColor.color(255, 255, 255))) //Do I need to explain what this does ?
+                    .append(Component.text(" was killed by " + teamKiller, TextColor.color(255, 255, 255))) //Do I need to explain what this does ?
                     .append(componentKiller) //Add custom killer head to message
                     .append(Component.text(' ' + killer.getName(), colorKiller)); //Add killer's name
             advMain.adventure().players().sendMessage(message); //Send message
